8affb87e05098953c68002775eea0518fd15739f
salieribaba <kanuniirfan@gmail.com>
2023-02-03 11:04:19 +0300

    part_006

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </div>
   );
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import { Fragment } from "react";
+import "./navigation.styles.scss";
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Mağaza
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
